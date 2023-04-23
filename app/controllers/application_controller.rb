3260fc831fe2c8b62455e755570e69273aea6720
sea-yassan33 <a.yasuda_tr51@me.com>
2023-04-23 17:00:32 +0900

    sign_out_link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:password_confirmation, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
