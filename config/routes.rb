f4b11fbf291b5f6c3c3640a2f4cf82563c2638be
sea-yassan33 <a.yasuda_tr51@me.com>
2023-04-30 08:03:19 +0900

    カード作成機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :tops
-  resources :lists, only: %i[new create edit update destroy]
+  resources :lists, only: %i[new create edit update destroy] do
+    resources :cards, only: %i[new create]
+  end
 end
