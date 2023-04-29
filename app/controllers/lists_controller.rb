528cb634cc8fdce2710b1e89f482451324032898
sea-yassan33 <a.yasuda_tr51@me.com>
2023-04-29 21:12:24 +0900

    list編集削除機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: %i[edit update destroy]
+
   def new
     @list = List.new
   end
@@ -12,9 +14,28 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @list.update(list_params)
+      redirect_to :root
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to :root
+  end
+
   private
   def list_params
     params.require(:list).permit(:title).merge(user: current_user)
+  end
+
+  def find_list
+    @list = List.find_by(id: params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :tops
-  resources :lists, only: %i[new create]
+  resources :lists, only: %i[new create edit update destroy]
 end
